89cf4ea0c6fbdae356daeb6fbb6b30a3cd08a08a
PatgioK <patgiok@gmail.com>
2022-04-02 18:58:05 -0700

    associate + user friend function separation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_02_231239) do
+ActiveRecord::Schema.define(version: 2022_04_03_004553) do
 
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2022_04_02_231239) do
     t.string "twitter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
